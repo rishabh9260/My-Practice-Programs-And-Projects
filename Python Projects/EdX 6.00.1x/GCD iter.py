62b0a4c2046ebc67b964c6a3e9d902b6a116e220
Rishabh Jain <rishabhjain9260@gmail.com>
2019-07-24 15:28:14 +0545

    Better way to imlement GCD iter

--- Python Projects/EdX 6.00.1x/GCD iter.py
+++ Python Projects/EdX 6.00.1x/GCD iter.py
@@ -3,17 +3,9 @@ def gcdIter(a, b):
     a, b: positive integers
     
     returns: a positive integer, the greatest common divisor of a & b.
-    '''
-    if a<b:
-        large = b
-        small = a
-    else:
-        large = a
-        small = b
-    
-    for x in range(small, 0, -1):
-        if large % x == 0 and small % x == 0:
-            print(x)
-            break
+    '''   
+    for x in range(min(a,b), 0, -1):
+        if a % x == 0 and b % x == 0:
+            return x
 
 gcdIter(35, 77)
