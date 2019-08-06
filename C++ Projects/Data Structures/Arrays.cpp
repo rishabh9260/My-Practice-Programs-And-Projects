b5acfcf5bef1fa1b1b886566f0b8eba81b901285
Rishabh Jain <rishabhjain9260@gmail.com>
2019-08-06 19:56:30 +0530

    Dynamic Array implemented

--- C++ Projects/Data Structures/Arrays.cpp
+++ C++ Projects/Data Structures/Arrays.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
 #include<vector>
+#include<stdlib.h>
+#include<conio.h>
 using namespace std;
 
-void arrayInsert(vector<char>, char a);
-char arraySearch(vector<char>, char a);
-void arrayDelete(vector<char>, char a);
-void arrayInsert(vector<int>, int a);
+void arrayInsert(vector<int>&, int a);
 int arraySearch(vector<int>, int a);
-void arrayDelete(vector<int>, int a);
+void arrayDelete(vector<int>&, int a);
+void displayArray(vector<int>);
 
 int main()
 {
     int ch = 0;
-    char a;
     int a;
     vector<int> arr;
-    vector<char> arr;
     do
     {   
         do
         {
-            cout<<"Arrays Program!!!\n\n";
+            cout<<endl;
+            system("pause");
+            system("cls");
+            cout<<"\nArrays Program!!!\n\n";
             cout<<"1. Insert in the array\n";
             cout<<"2. Search a element in the array\n";
             cout<<"3. Delete from the array\n";
-            cout<<"4. Exit\n";
+            cout<<"4. Display elements of the array\n";
+            cout<<"5. Exit\n";
             cout<<"\nEnter your choice:";
             cin>>ch;
-            if(ch < 1 || ch > 4)
-                cout<<"Invalid Choice!! Try again";
-        }while(ch < 1 || ch > 4);
+            if(ch < 1 || ch > 5)
+                cout<<"Invalid Choice!! Try again\n\n";
+        }while(ch < 1 || ch > 5);
 
         switch(ch)
         {
@@ -39,96 +41,79 @@ int main()
                     break;
             case 2: cout<<"Enter the element to be searched:";
                     cin>>a;
-                    int flag = arraySearch(arr,a);
-                    if(flag == NULL || flag == -1)
+                    int flag; 
+                    flag = arraySearch(arr,a);
+                    if(flag == -1)
                         cout<<"\nElement Not Found!";
                     else
                     {
-                        cout<<"\nElement found at index "<<flag;
+                        cout<<"\nElement found at index "<<flag<<endl;
                     }
                     break;
             case 3: cout<<"\nEnter element to be deleted:";
                     cin>>a;
-                    arrayDelete(arr,a);
+                    arrayDelete(arr, a);
                     break;
-            case 4: cout<<"Exiting Program!!!";
+            case 4: displayArray(arr);
+                    break;
+            case 5: cout<<"Exiting Program!!!";
                     exit(0);
         }
-    }while(ch >= 1 || ch <= 4);
+    }while(ch >= 1 || ch <= 5);
 
     return 0;
 }
 
-void arrayInsert(vector<char> arr, char a)
+void arrayInsert(vector<int> &arr, int a)
 {
     arr.push_back(a);
-}
-
-void arrayInsert(vector<int> arr, int a)
-{
-    arr.push_back(a);
-}
-
-char arraySearch(vector<char> arr, char a)
-{
-    for(int i = 0; i<arr.size(); ++i)
-    {
-        if(arr[i] == a)
-        {
-            return i;
-        }
-        else
-        {
-            return NULL;
-        }
-        
-    }
 }
 
 int arraySearch(vector<int> arr, int a)
 {
-    for(int i = 0; i<arr.size(); ++i)
+    for(int i = 0; i<arr.size(); i++)
     {
-        if(arr[i] == a)
+        if(arr.at(i) == a)
         {
             return i;
         }
         else
         {
-            return -1;
+            continue;
         }
         
     }
+    return -1;
 }
 
-void arrayDelete(vector<char> arr, char a)
+void arrayDelete(vector<int> &arr, int a)
 {
-    for(vector<char>::iterator i = arr.begin(); i<arr.end(); ++i)
-    {
-        if(*i == a)
-        {
-            arr.erase(i);
-        }
-        else
-        {
-            cout<<"\nElement does not exist in the array!";
-        }
-        
-    }
-}
-
-void arrayDelete(vector<int> arr, int a)
-{
+    int flag = 0;
     for(vector<int>::iterator i = arr.begin(); i<arr.end(); ++i)
     {
         if(*i == a)
         {
             arr.erase(i);
-        }
-        else
+            flag = 1;
+            break;
+        }        
+    }
+    if(flag == 0)
+        cout<<"\nElement does not exist in the array!!";
+}
+
+void displayArray(vector<int> arr)
+{
+    if(arr.empty())
+    {
+        cout<<"\nArray is empty!";
+    }
+    else
+    {    
+        cout<<"\nDisplaying the array!\n";
+        for(int i = 0; i<arr.size(); i++)
         {
-            cout<<"\nElement does not exist in the array!";
+            cout<<arr[i]<<"\t";
         }
-        
     }
 }
