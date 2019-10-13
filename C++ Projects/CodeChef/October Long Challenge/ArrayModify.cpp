7222873fc552fb516607d2455f9c94266565db20
Rishabh Jain <rishabhjain9260@gmail.com>
2019-10-13 17:15:15 +0530

    ArrayModify accepted

--- C++ Projects/CodeChef/October Long Challenge/ArrayModify.cpp
+++ C++ Projects/CodeChef/October Long Challenge/ArrayModify.cpp
@@ -7,22 +7,53 @@ int main()
     cin>>t;
     while(t--)
     {
-        int n,k,a,b;
+        int n,a,b;
+        long long int k;
         cin>>n>>k;
-        int arr[n];
+        int final[n],copy[n],xor_arr[n/2];
         for(int i=0;i<n;i++)
         {
-            cin>>arr[i];
+            cin>>final[i];
+            copy[i] = final[i];
         }
-        for(int i=0;i<k;i++)
+        for(int i=0;i<n/2;i++)
         {
-            a=arr[i%n];
-            b=arr[n-(i%n)-1];
-            arr[i%n]=a^b;
+            a=final[i%n];
+            b=final[n-(i%n)-1];
+            xor_arr[i%n]=a^b;
+        }
+        if(k>=n)
+        {
+            if(((k/n)-1)%3 == 0)
+            {
+                for(int i=0;i<n/2;i++)
+                {
+                    final[i%n] = xor_arr[i%n];
+                    final[n-(i%n)-1] = copy[i%n];
+                }
+            }
+            else if(((k/n)-2)%3 == 0)
+            {
+                for(int i=0;i<n/2;i++)
+                {
+                    final[n-(i%n)-1] = xor_arr[i%n];
+                    final[i%n] = copy[n-(i%n)-1];
+                }
+            }
+            if(n%2 == 1)
+            {
+               final[n/2] = 0; 
+            }
+        }
+        for(int i=0;i<k%n;i++)
+        {
+            a=final[i%n];
+            b=final[n-(i%n)-1];
+            final[i%n]=a^b;
         }
         for(int i=0;i<n;i++)
         {
-            cout<<arr[i]<<" ";
+            cout<<final[i]<<" ";
         }
     }
 }
