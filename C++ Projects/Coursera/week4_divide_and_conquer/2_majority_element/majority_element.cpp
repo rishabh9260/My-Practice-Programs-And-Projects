3324011b6b814104c926fb4f1d728cfac935d862
Rishabh Jain <rishabhjain9260@gmail.com>
2020-05-24 13:41:05 +0545

    majority element problem solved

--- C++ Projects/Coursera/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ C++ Projects/Coursera/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -5,10 +5,27 @@
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
+  if (left == right) return 0;
+  if (left + 1 == right) return 1;
   //write your code here
-  return -1;
+  sort(a.begin(),a.end());
+  int cur = 0;
+  int count = 0;
+  for(int i=left;i<right;i++)
+  {
+    if(a[i] != cur)
+    {
+      cur = a[i];
+      count = 1;
+    }
+    else
+    {
+      count++;
+    }
+    if(count>(right/2))
+      return 1;
+  }
+  return 0;
 }
 
 int main() {
@@ -18,5 +35,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout <<get_majority_element(a, 0, a.size());
 }
